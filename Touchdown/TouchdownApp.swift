596c9719ddca5690b50072c843ada1eec7b37070
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-19 21:32:57 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Lori Rothermel on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
